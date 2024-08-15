618fb6d862020c16e49bebae1f6abb5e8f4da25a
김민재 <kimminjae@gimminjaeui-MacBookPro.local>
2024-08-15 16:22:19 +0900

    feat: 커뮤니티 api  구현

---
+++ backend/server.js
@@ -0,0 +1,156 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+var cors = require('cors');
+app.use(cors());
+app.use(express.urlencoded({extended: true})); //req.body 쓰려면 이코드가 필요
+
+//  몽고디비연결
+const { MongoClient, ObjectId } = require('mongodb');
+
+let db;
+const url = "mongodb+srv://mj950313:100489jae@minjae.mpxmk1e.mongodb.net/?retryWrites=true&w=majority&appName=minjae";
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+new MongoClient(url).connect().then(()=>{
+    console.log("DB연결성공")
+    db = client.db("danak");
+    app.listen(8080, function () {
+        console.log('Server is running on port 8080');
+    });
+}).catch((err)=>{
+    console.log(err)
+})
+
+
+
+const products = [
+    { id: 1, name: "BIXOD N MOUNTAIN STREAM", description: "(빅소드 엔 마운틴 스트림)", price: "290,000원", image: "/path/to/image1.jpg", category: "신제품" },
+    { id: 2, name: "INK N AIR", description: "잉크 엔 에어 쭈꾸미 갑오징어 낚싯대", price: "330,000원", image: "/path/to/image2.jpg", category: "신제품" },
+    { id: 3, name: "40주년 로드", description: "40주년 한정판)배스,참돔,오징어,갑오징어,광어", price: "410,000원", image: "/path/to/image3.jpg", category: "바다" },
+    { id: 4, name: "BIXOD N BLACK LABEL", description: "(BIXOD N BLACK LABEL)배스", price: "500,000원", image: "/path/to/image4.jpg", category: "민물" },
+    { id: 5, name: "BIXOD N BLACK LABEL", description: "(BIXOD N BLACK LABEL)배스", price: "500,000원", image: "/path/to/image4.jpg", category: "루어" },
+  ];
+  
+
+// API
+app.get("/api/products", (req, res) => {
+    res.json(products)
+})
+
+//커뮤니티 API
+app.get("/api/community", async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1; // 요청된 페이지 번호 (기본값 1)
+        const limit = 10; // 한 페이지에 보여줄 글의 수
+        const skip = (page - 1) * limit; // 스킵할 문서 수 계산
+
+        const totalPosts = await db.collection("community").countDocuments(); // 전체 게시물 수
+        const totalPages = Math.ceil(totalPosts / limit); // 전체 페이지 수
+
+        const response = await db.collection("community")
+            .find({})
+            .sort({ createdAt: -1 })
+            .skip(skip)
+            .limit(limit)
+            .toArray();
+
+        res.json({
+            posts: response,
+            totalPages, // 전체 페이지 수 반환
+            currentPage: page, // 현재 페이지
+        });
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+        console.error("Database query failed:", error);
+    }
+});
+
+app.get("/api/community/:id", async (req, res) => {
+     try {
+        const id = req.params.id;
+        const response = await db.collection("community").findOne({ _id: new ObjectId(id) });
+        res.json(response);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+        console.error("Database query failed:", error);
+    }
+});
+
+app.delete("/api/community/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const result = await db.collection("community").deleteOne({ _id: new ObjectId(id) });
+
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ error: "Story not found" });
+        }
+
+        res.send("글 삭제 완료");
+    } catch (error) {
+        console.error("Error deleting post:", error);
+        res.status(500).send("글 삭제 중 서버에러가 발생했습니다.");
+    }
+});
+
+app.put("/api/community/write/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const updateData = {
+            title: req.body.title,
+            content: req.body.content,
+            updatedAt: new Date()
+        };
+
+        const result = await db.collection("community").updateOne(
+            { _id: new ObjectId(id) },
+            { $set: updateData }
+        );
+
+        if (result.matchedCount === 0) {
+            return res.status(404).json({ error: "Story not found" });
+        }
+
+        res.send("글 수정 완료");
+    } catch (error) {
+        console.error("Error updating post:", error);
+        res.status(500).send("글 수정 중 서버에러가 발생했습니다.");
+    }
+});
+
+app.post("/api/community/write", async (req, res) => {
+    if (req.body.title.trim() === "") {
+     return res.status(400).send("제목을 입력해주세요");
+    }
+    if (req.body.content.trim() === "") {
+     return res.status(400).send("내용을 입력해주세요");
+    }
+    try {
+        const currentTime = new Date();
+
+        await db.collection("community").insertOne({
+            title: req.body.title,
+            content: req.body.content,
+            createdAt: currentTime,
+            updatedAt: currentTime 
+        });
+        res.send("글 작성 완료");
+    } catch (error) {
+        console.error("Error writing post:", error);
+        res.status(500).send("글 작성 중 서버에러가 발생했습니다.");
+    }
+ });
+ 
+
+
+
+// 리액트 라우터사용
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+})
